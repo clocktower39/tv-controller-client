e2593f8c5322f75b54ea5bb9f5a7060c6b9ba0c1
clocktower39 <clocktower39@aol.com>
2025-04-20 12:55:50 -0700

    adjust ControllerKeyButton component, upgrade packages, adjust Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Container, IconButton, Grid } from "@mui/material";
 import {
   PowerSettingsNew,
@@ -30,89 +30,124 @@ import "./App.css";
 const serverURL = `http://192.168.0.12:8000`;
 
 function App() {
-
   const handleClick = (type, key) => {
     fetch(`${serverURL}/`, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({ type, key }) // body data type must match "Content-Type" header
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({ type, key }), // body data type must match "Content-Type" header
     });
-  }
+  };
 
-  const controllerKeyButton = (buttonCommand, icon, gridSize = 2) => {
+  const ControllerKeyButton = ({ buttonCommand, icon, gridSize = 2 }) => {
     return (
-      <Grid item xs={gridSize} container className="JCAICenter" >
-        <IconButton
-          onClick={() => handleClick('key', buttonCommand)}
-        >
-          {icon}
-        </IconButton>
+      <Grid container size={gridSize} className="JCAICenter">
+        <IconButton onClick={() => handleClick("key", buttonCommand)}>{icon}</IconButton>
       </Grid>
     );
-  }
+  };
 
   return (
-    <Container maxWidth="sm" style={{ height: '100%', }} >
-      <Grid container className="JCAICenter" style={{ height: '100%', }} >
-
-        <Grid container className="JCAICenter" >
-
-          {controllerKeyButton('setup-menu', <Settings fontSize="large" />)}
-          {controllerKeyButton('input-select', <Input fontSize="large" />)}
-          {controllerKeyButton('power-toggle-function', <PowerSettingsNew fontSize="large" />)}
-
+    <Container maxWidth="sm" style={{ height: "100%" }}>
+      <Grid container className="JCAICenter" style={{ height: "100%" }}>
+        <Grid container className="JCAICenter" size={12}>
+          <ControllerKeyButton buttonCommand="setup-menu" icon={<Settings fontSize="large" />} />
+          <ControllerKeyButton buttonCommand="input-select" icon={<Input fontSize="large" />} />
+          <ControllerKeyButton
+            buttonCommand="power-toggle-function"
+            icon={<PowerSettingsNew fontSize="large" />}
+          />
         </Grid>
 
-        <Grid container className="JCAICenter" >
-
-          {controllerKeyButton('backward', <SkipPrevious sx={{ fontSize: '50px' }} />)}
-          {controllerKeyButton('rewind', <FastRewind fontSize="large" />)}
-          {controllerKeyButton('play', <PlayArrow fontSize="large" />)}
-          {controllerKeyButton('pause', <Pause fontSize="large" />)}
-          {controllerKeyButton('forward', <FastForward fontSize="large" />)}
-          {controllerKeyButton('fast-forward', <SkipNext fontSize="large" />)}
-
+        <Grid container size={12} className="JCAICenter">
+          <ControllerKeyButton
+            buttonCommand="backward"
+            icon={<SkipPrevious sx={{ fontSize: "50px" }} />}
+          />
+          <ControllerKeyButton buttonCommand="rewind" icon={<FastRewind fontSize="large" />} />
+          <ControllerKeyButton buttonCommand="play" icon={<PlayArrow fontSize="large" />} />
+          <ControllerKeyButton buttonCommand="pause" icon={<Pause fontSize="large" />} />
+          <ControllerKeyButton buttonCommand="forward" icon={<FastForward fontSize="large" />} />
+          <ControllerKeyButton buttonCommand="fast-forward" icon={<SkipNext fontSize="large" />} />
         </Grid>
 
-        <Grid container className="JCAICenter" >
-
-          <Grid container className="JCAICenter" >
-            {controllerKeyButton('up', <ArrowDropUp sx={{ fontSize: '120px' }} />, 12)}
+        <Grid container size={12} className="JCAICenter">
+          <Grid container size={12} className="JCAICenter">
+            <ControllerKeyButton
+              buttonCommand="up"
+              icon={<ArrowDropUp sx={{ fontSize: "120px" }} />}
+              gridSize={12}
+            />
           </Grid>
 
-          <Grid container className="JCAICenter" >
-            {controllerKeyButton('left', <ArrowLeft sx={{ fontSize: '120px' }} />, 4)}
-            {controllerKeyButton('select', <Circle sx={{ fontSize: '75px' }} />)}
-            {controllerKeyButton('right', <ArrowRight sx={{ fontSize: '120px' }} />, 4)}
+          <Grid container size={12} className="JCAICenter">
+            <ControllerKeyButton
+              buttonCommand="left"
+              icon={<ArrowLeft sx={{ fontSize: "120px" }} />}
+              gridSize={4}
+            />
+            <ControllerKeyButton
+              buttonCommand="select"
+              icon={<Circle sx={{ fontSize: "75px" }} />}
+            />
+            <ControllerKeyButton
+              buttonCommand="right"
+              icon={<ArrowRight sx={{ fontSize: "120px" }} />}
+              gridSize={4}
+            />
           </Grid>
 
-          <Grid container className="JCAICenter" >
-            {controllerKeyButton('down', <ArrowDropDown sx={{ fontSize: '120px' }} />, 4)}
+          <Grid container size={12} className="JCAICenter">
+            <ControllerKeyButton
+              buttonCommand="down"
+              icon={<ArrowDropDown sx={{ fontSize: "120px" }} />}
+              gridSize={4}
+            />
           </Grid>
-
         </Grid>
 
-        <Grid container className="JCAICenter" >
-            {controllerKeyButton('volume-down', <VolumeDown fontSize="large" /> , 3)}
-            {controllerKeyButton('mute', <VolumeOff fontSize="large" /> , 3)}
-            {controllerKeyButton('volume-up', <VolumeUp fontSize="large" /> , 3)}
+        <Grid container size={12} className="JCAICenter">
+          <ControllerKeyButton
+            buttonCommand="volume-down"
+            icon={<VolumeDown fontSize="large" />}
+            gridSize={3}
+          />
+          <ControllerKeyButton
+            buttonCommand="mute"
+            icon={<VolumeOff fontSize="large" />}
+            gridSize={3}
+          />
+          <ControllerKeyButton
+            buttonCommand="volume-up"
+            icon={<VolumeUp fontSize="large" />}
+            gridSize={3}
+          />
         </Grid>
 
-        <Grid container className="JCAICenter" >
-            {controllerKeyButton('exit', <UTurnLeft fontSize="large" /> , 3)}
-            {controllerKeyButton('root-menu', <Home fontSize="large" /> , 3)}
-            {controllerKeyButton('contents-menu', <Menu fontSize="large" /> , 3)}
+        <Grid container size={12} className="JCAICenter">
+          <ControllerKeyButton
+            buttonCommand="exit"
+            icon={<UTurnLeft fontSize="large" />}
+            gridSize={3}
+          />
+          <ControllerKeyButton
+            buttonCommand="root-menu"
+            icon={<Home fontSize="large" />}
+            gridSize={3}
+          />
+          <ControllerKeyButton
+            buttonCommand="contents-menu"
+            icon={<Menu fontSize="large" />}
+            gridSize={3}
+          />
         </Grid>
-
-
-      </Grid >
+      </Grid>
     </Container>
   );
 }
